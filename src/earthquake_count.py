dd3e941eec90760ec18a17fec117545b03c29b1f
truongdh3-vcu <truongdh3@vcu.edu>
2026-09-09 20:38:25 +0000

    added some comments

--- src/earthquake_count.py
+++ src/earthquake_count.py
@@ -1,5 +1,5 @@
 import requests
-
+#this is a lot simpler it basically requests data based on my parameters and gives me a number
 url = "https://earthquake.usgs.gov/fdsnws/event/1/count"
 
 parameters = { #these are the parameters by which the api is following
--- src/earthquake_query.py
+++ src/earthquake_query.py
@@ -4,18 +4,18 @@ import requests
 url = "https://earthquake.usgs.gov/fdsnws/event/1/query"
 
 parameters = { #these are the parameters by which the api is following
-    "format": "geojson",
+    "format": "geojson", #gives the results in geojson format
     "minmagnitude": 2.5,
     "orderby": "time",
     "limit": 10
 }
 
-response = requests.get(url, params=parameters)
+response = requests.get(url, params=parameters) #this is what initiates the request
 
 data = response.json()
 
-for earthquake in data["features"]:
+for earthquake in data["features"]: #basically a loop that says for each earthquake in "features" (where the earthquake data is stored), print its place and magnitude
     print(earthquake["properties"]["place"]) #prints the properties of each earthquake (ie location and distance from major city)
     print(earthquake["properties"]["mag"]) #prints the magnitude of each earthquake (should be a min of 2.5)
-    print()
+    print() #just an extra space for readability
 
