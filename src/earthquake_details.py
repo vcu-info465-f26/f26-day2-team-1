081944997604c933a69e1b3ada08877d68fda788
hiraVCU <shakeelh2@vcu.edu>
2026-09-10 18:41:24 +0000

    added function to get specific earthquake details

--- src/earthquake_details.py
+++ src/earthquake_details.py
@@ -1,13 +1,34 @@
 import requests
-url = "https://earthquake.usgs.gov/fdsnws/event/1/query"
-parameters = {
-    "format": "geojson",
-    "eventid": "us7000tgb4"
-}
-response= requests.get(url, params=parameters)
-data = response.json()
 
-print(data.keys())
-print(data["properties"].keys())
-print(data)
+def get_earthquake_details(event_id):
+    url = "https://earthquake.usgs.gov/fdsnws/event/1/query"
 
+    parameters = {
+        "format": "geojson",
+        "eventid": event_id
+    }
+    response= requests.get(url, params=parameters)
+    data = response.json()
+
+    print(data.keys())
+    print(data["properties"].keys())
+    
+
+    properties = data["properties"]
+    coordinates = data["geometry"]["coordinates"]
+
+    details = {
+        "id": data["id"],
+        "status": properties["status"],
+        "tsunami": properties["tsunami"],
+        "significance": properties["sig"],
+        "stations": properties["nst"],
+        "magnitude_type": properties["magType"],
+        "depth_km": coordinates[2]
+    }
+    return details
+
+if __name__== "__main__":
+    details = get_earthquake_details("us7000tgb4")
+for key, value in details.items():
+    print(f"{key}:{value}")
